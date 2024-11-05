fb2e8c51814f8c17cc7703bffc2cf5af7ef604e2
Cícero Feijó <cicero.feijo@gmail.com>
2024-11-05 19:57:54 -0300

    aula 02: sintaxe jsx/tsx

--- app-react/src/App.tsx
+++ app-react/src/App.tsx
@@ -3,7 +3,10 @@ import './App.css'
 function App() {
 
   const welcome = <h1>Bem vindo ao curso</h1>
-  const gostando = true;
+  const gostando = false
+  const numero = 2
+  const cursos = ['HTML', 'CSS', 'JS']
+
   // Continuamos daqui amanha
 
   return (
@@ -14,6 +17,25 @@ function App() {
         <h1>Aula 01</h1>
         {welcome}
         <h2>Introdução ao React</h2>
+        { gostando ? 'Estou curtindo' : <h3>NAO CURTI</h3> }
+
+        <p className={numero % 2 ? 'impar' : 'par'}>{numero}</p>
+
+        {gostando &&
+          <div>
+            <h3>Estou curtindo muito</h3>        
+          </div>
+        }
+
+        {cursos.map((curso) => {
+          return <p>{curso}</p>
+        })}
+
+        {/* Exemplo resumido */}
+        {/* {cursos.map(c => <p>{c}</p>)} */}
+
+
+
       </div>
       
 

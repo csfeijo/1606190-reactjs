42f9241d82a68e5d087c4d88d06971a3705c7b26
Cícero Feijó <cicero.feijo@gmail.com>
2024-11-07 22:02:43 -0300

    aula 03: react hooks (useState e useEffect)

--- README.md
+++ README.md
@@ -19,7 +19,53 @@ Turma de ReactJS da Targettrust.
 - [x] Childrens
 - [x] Interface em TS
 
+## Aula 03
+
+- [x] Exercício: Componente de botão
+- [x] React Hooks
+- [x] useState
+- [x] useEffect
+
 # Links uteis
 
 - [State of JS](https://stateofjs.com/en-US)
 - [State of CSS](https://stateofcss.com/en-US)
+
+- [4 Maneiras de Aprimorar Seus IFS](https://www.linkedin.com/posts/isaac-gomes-matos_typescript-javascript-activity-7260263756784435200-wEwr)
+
+- [GitHub Handbook](https://www.linkedin.com/posts/awdhesh-kumar-27427424b_gitgithub-mastery-streamline-your-version-ugcPost-7259194299597086720-P3K1)
+
+- [Front End Enginner - Dicas CSS](https://www.linkedin.com/in/arbaoui-mehdi-793a96154/)
+
+# Sobre o destructuring
+
+Pode ser realizado sobre um retorno de objeto ou de array.
+Exemplo:
+
+**Objeto**
+
+```js
+const pessoa = {
+  nome: "Cícero",
+  idade: 43,
+  cidade: "Porto Alegre",
+};
+
+// Destructuring para extrair "nome" e "cidade"
+const { nome, cidade } = pessoa;
+
+console.log(nome); // Saída: Cícero
+console.log(cidade); // Saída: Porto Alegre
+```
+
+**Array**
+
+```js
+const numeros = [10, 20, 30, 40];
+
+// Destructuring para extrair os dois primeiros elementos
+const [primeiro, segundo] = numeros;
+
+console.log(primeiro); // Saída: 10
+console.log(segundo); // Saída: 20
+```
--- app-react/src/App.tsx
+++ app-react/src/App.tsx
@@ -1,27 +1,66 @@
-import "./App.css";
+import { useEffect, useState } from "react";
+import "./App.scss";
 import Titulo from "./Components/Titulo";
 import Placeholder from "./Components/Placeholder";
+import Botao from "./Components/Botao";
 
 const App = () => {
+  // destructuring
+  const [numero, setNumero] = useState(0);
+  const [nome, setNome] = useState("");
+
+  useEffect(() => {
+    if (numero !== 0) {
+      console.log("Entrou no effect do numero");
+    }
+  }, [numero]);
+
+  useEffect(() => {
+    if (nome !== "") {
+      console.log("Entrou no effect do nome");
+    }
+  }, [nome]);
+
   return (
     <>
+      {nome !== "" && <p>Você está logado como {nome}</p>}
+      <h1>{numero}</h1>
       <Titulo
         nome="ReactJS"
-        descricao="curso introdutório"
+        descricao={`curso introdutório ${numero}`}
         data={new Date(2024, 11, 5)}
       >
         <Placeholder />
       </Titulo>
 
-      <Titulo
-        nome="ReactJS"
-        descricao="curso introdutório"
-        data={new Date(2024, 11, 5)}
-      >
-        <div>ola pessoal! aula 02</div>
-      </Titulo>
+      <h1>Aula 03</h1>
 
-      <h1>Aula 02</h1>
+      {nome !== "" && <h4>Bem vindo, {nome}</h4>}
+
+      <Botao label="Primário" severity="primary" />
+
+      <Botao
+        label="Bota o nome"
+        severity="warning"
+        onClick={() => {
+          setNome("Cícero Feijó");
+        }}
+      />
+
+      <Botao
+        label="-1"
+        severity="danger"
+        onClick={() => {
+          setNumero(numero - 1);
+        }}
+      />
+      <Botao
+        label="+1"
+        severity="danger"
+        onClick={() => {
+          setNumero(numero + 1);
+        }}
+      />
     </>
   );
 };

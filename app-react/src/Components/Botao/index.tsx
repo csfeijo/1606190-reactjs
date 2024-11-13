9ce8ca0b5cb2fe656a62286148749839b840e71f
Cícero Feijó <cicero.feijo@gmail.com>
2024-11-12 21:44:51 -0300

    aula 05: styled components no botao e no titulo

--- app-react/src/Components/Botao/index.tsx
+++ app-react/src/Components/Botao/index.tsx
@@ -1,19 +1,20 @@
-import "./botao.scss";
-
 interface BotaoProps {
   label: string;
   severity: "danger" | "primary" | "secondary" | "warning";
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  className?: string;
 }
 
-const Botao = ({ label, severity, onClick }: BotaoProps) => {
+const Botao = ({ className, label, severity, onClick }: BotaoProps) => {
   return (
-    <>
-      <button className={severity} onClick={onClick}>
-        {label}
-      </button>
-    </>
+    <button className={`${className} ${severity}`} onClick={onClick}>
+      {label}
+    </button>
   );
 };
+
+// const Botao = ({ className }: any) => {
+//   return <button className={className}>TESTE 1</button>;
+// };
 
 export default Botao;

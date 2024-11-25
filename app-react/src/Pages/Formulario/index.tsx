20792b35d3c00a3a4ed77dfbf8563fc77f706188
Cícero Feijó <cicero.feijo@gmail.com>
2024-11-25 20:39:25 -0300

    aula 10: correção do exercicio de post do formulario

--- app-react/src/Pages/Formulario/index.tsx
+++ app-react/src/Pages/Formulario/index.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import BotaoCustom from "../../Components/Botao/botao.styled";
+import api from "../../Services/api";
 interface InterfaceFormulario {
   className?: string;
 }
@@ -8,6 +10,7 @@ const Formulario = ({ className }: InterfaceFormulario) => {
   const [nome, setNome] = useState("");
   const [sigla, setSigla] = useState("");
   const [mensagem, setMensagem] = useState("");
+  const navigate = useNavigate();
 
   return (
     <div className={className}>
@@ -51,7 +54,7 @@ const Formulario = ({ className }: InterfaceFormulario) => {
         <BotaoCustom
           severity="primary"
           label="SALVAR"
-          onClick={() => {
+          onClick={async () => {
             setMensagem("");
 
             if (nome === "") {
@@ -62,6 +65,20 @@ const Formulario = ({ className }: InterfaceFormulario) => {
             if (sigla === "") {
               setMensagem("Preencha a sigla corretamente");
               return;
+            }
+
+            //Só vou chegar se tiver passado tudo OK na validacao
+            try {
+              await api.post("/departamentos", {
+                nome,
+                sigla,
+              });
+              // Redirecionar o usuario para a lista
+              navigate("/listagem");
+            } catch (e: any) {
+              const erro = e.response?.data?.message;
+
+              setMensagem(`[${erro}] Nome ou Sigla já existentes.`);
             }
           }}
         />
